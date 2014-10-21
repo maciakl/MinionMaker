6bfb54a91c6ad209139a9a5c4391f3f31872c817
Luke Maciak <luke@maciak.net>
2014-10-21 16:07:59 -0400

    Added descriptors

--- app.rb
+++ app.rb
@@ -10,17 +10,19 @@ class MinionFactory
 
     minion = Minion.new()
 
+    #minion.race = 'Kobold'
     minion.race   = Pickup.new(@data['race']).pick(1)
     minion.gender = Pickup.new(@data['gender']).pick(1)
     minion.class  = Pickup.new(@data['class']).pick(1)
     minion.social  = Pickup.new(@data['social']).pick(1)
     #minion.class = 'Cleric'
 
-    minion.primary_weapon = self.pick_weapon(minion.class, 'primary_weapon')
-    minion.secondary_weapon = self.pick_weapon(minion.class, 'secondary_weapon')
+    minion.primary_weapon = self.indirect_category_pick(minion.class, 'primary_weapon')
+    minion.secondary_weapon = self.indirect_category_pick(minion.class, 'secondary_weapon')
+    minion.armor = self.category_pick(minion.class, 'armor')
     
-    loadout = (@data.include? minion.class and @data[minion.class].include? 'armor') ? minion.class : 'Default'
-    minion.armor  = Pickup.new(@data[loadout]['armor']).pick(1)
+    #loadout = (@data.include? minion.class and @data[minion.class].include? 'armor') ? minion.class : 'Default'
+    #minion.armor  = Pickup.new(@data[loadout]['armor']).pick(1)
 
     minion.traits = Pickup.new(@data['traits']['common']).pick(3)
     minion.traits = minion.traits.uniq.reject(&:empty?).join(', ')
@@ -31,7 +33,6 @@ class MinionFactory
     minion.hates = "other #{minion.hates}" if minion.hates == minion.race+'s'
     minion.hates = Pickup.new(@data['class']).pick(1)+'s' if minion.hates == 'class'
     minion.hates = "other #{minion.hates}" if minion.hates == minion.class+'s'
-    minion.hates = "" if minion.hates == 'nothing'
     minion.hates = "Hates #{minion.hates}.\n" unless minion.hates.empty?
 
 
@@ -39,17 +40,30 @@ class MinionFactory
     minion.fears = "" if minion.fears == 'nothing'
     minion.fears = "Fears #{minion.fears}.\n" unless minion.fears.empty?
 
+    minion.eyes = self.category_pick(minion.race, 'eyes')
+    minion.hair = self.category_pick(minion.race, 'hair_style')
+    minion.hair += ", #{self.category_pick(minion.race, 'hair_color')}" unless minion.hair == 'shaved head' or minion.hair == 'bald'
+    minion.skin = self.category_pick(minion.race, 'skin')
+
+
     return minion
 
   end
 
-  def pick_weapon(rpg_class, type)
+  def category_pick(category, item)
+    category = @data[category]['same_as'] if @data.include? category and @data[category].include? 'same_as'
+    current_loadout = default_loadout = @data['Default']
+    current_loadout = @data[category] if @data.include? category and @data[category].include? item
+    Pickup.new(current_loadout[item]).pick(1)
+  end
+
+  def indirect_category_pick(rpg_class, type)
     current_loadout = default_loadout = @data['Default']
     current_loadout = @data[rpg_class] if @data.include? rpg_class and @data[rpg_class].include? type
 
     tmp = Pickup.new(current_loadout[type]).pick(1)
 
-    if tmp == 'none'
+    if tmp.empty?
       weapon = ''
     else
       current_loadout = default_loadout unless current_loadout.include? tmp
@@ -71,6 +85,10 @@ class Minion
   attr_accessor :hates
   attr_accessor :fears
 
+  attr_accessor :eyes
+  attr_accessor :hair
+  attr_accessor :skin
+
   attr_accessor :primary_weapon
   attr_accessor :secondary_weapon
   attr_accessor :armor
@@ -82,6 +100,9 @@ class Minion
     "#{@traits}"+
     "#{@hates}"+
     "#{@fears}"+
+    "#{@eyes.capitalize} eyes, #{@skin} skin.\n"+
+    "Hair: #{@hair}.\n"+
+
     "Weapons: "+ [@primary_weapon, @secondary_weapon].reject(&:empty?).join(", ")+"\n"+
     "Armor: #{@armor}\n"
   end
